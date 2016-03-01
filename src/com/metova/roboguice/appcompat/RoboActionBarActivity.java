7803680b41c147500afcab24f684053562d3a2b6
Brad Gearon <bradgearon@gmail.com>
2016-02-29 23:43:24 -0600

    updated for latest RoboGuice switched to AppCompatActivity

--- src/com/metova/roboguice/appcompat/RoboActionBarActivity.java
+++ src/com/metova/roboguice/appcompat/RoboActionBarActivity.java
@@ -4,30 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import roboguice.RoboGuice;
-import roboguice.activity.event.OnActivityResultEvent;
-import roboguice.activity.event.OnConfigurationChangedEvent;
-import roboguice.activity.event.OnContentChangedEvent;
-import roboguice.activity.event.OnCreateEvent;
-import roboguice.activity.event.OnDestroyEvent;
-import roboguice.activity.event.OnNewIntentEvent;
-import roboguice.activity.event.OnPauseEvent;
-import roboguice.activity.event.OnRestartEvent;
-import roboguice.activity.event.OnResumeEvent;
-import roboguice.activity.event.OnStartEvent;
-import roboguice.activity.event.OnStopEvent;
+import roboguice.activity.event.*;
+import roboguice.context.event.*;
 import roboguice.event.EventManager;
 import roboguice.inject.ContentViewListener;
 import roboguice.inject.RoboInjector;
 import roboguice.util.RoboContext;
+
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.inject.Inject;
 import com.google.inject.Key;
 
-public class RoboActionBarActivity extends ActionBarActivity implements RoboContext {
+public class RoboActionBarActivity extends AppCompatActivity implements RoboContext {
 
     protected EventManager eventManager;
     protected HashMap<Key<?>, Object> scopedObjects = new HashMap<Key<?>, Object>();
@@ -36,57 +28,56 @@ public class RoboActionBarActivity extends ActionBarActivity implements RoboCont
     ContentViewListener ignored; // BUG find a better place to put this
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
+    protected void onCreate(Bundle savedInstanceState) {
 
-        final RoboInjector injector = RoboGuice.getInjector( this );
-        eventManager = injector.getInstance( EventManager.class );
-        injector.injectMembersWithoutViews( this );
-        super.onCreate( savedInstanceState );
-        eventManager.fire( new OnCreateEvent( savedInstanceState ) );
+        final RoboInjector injector = RoboGuice.getInjector(this);
+        eventManager = injector.getInstance(EventManager.class);
+        injector.injectMembersWithoutViews(this);
+        super.onCreate(savedInstanceState);
+        eventManager.fire(new OnCreateEvent<>(this, savedInstanceState));
     }
 
     @Override
     protected void onRestart() {
 
         super.onRestart();
-        eventManager.fire( new OnRestartEvent() );
+        eventManager.fire(new OnRestartEvent(this));
     }
 
     @Override
     protected void onStart() {
 
         super.onStart();
-        eventManager.fire( new OnStartEvent() );
+        eventManager.fire(new OnStartEvent<>(this));
     }
 
     @Override
     protected void onResume() {
 
         super.onResume();
-        eventManager.fire( new OnResumeEvent() );
+        eventManager.fire(new OnResumeEvent(this));
     }
 
     @Override
     protected void onPause() {
 
         super.onPause();
-        eventManager.fire( new OnPauseEvent() );
+        eventManager.fire(new OnPauseEvent(this));
     }
 
     @Override
-    protected void onNewIntent( Intent intent ) {
+    protected void onNewIntent(Intent intent) {
 
-        super.onNewIntent( intent );
-        eventManager.fire( new OnNewIntentEvent() );
+        super.onNewIntent(intent);
+        eventManager.fire(new OnNewIntentEvent(this));
     }
 
     @Override
     protected void onStop() {
 
         try {
-            eventManager.fire( new OnStopEvent() );
-        }
-        finally {
+            eventManager.fire(new OnStopEvent(this));
+        } finally {
             super.onStop();
         }
     }
@@ -95,39 +86,37 @@ public class RoboActionBarActivity extends ActionBarActivity implements RoboCont
     protected void onDestroy() {
 
         try {
-            eventManager.fire( new OnDestroyEvent() );
-        }
-        finally {
+            eventManager.fire(new OnDestroyEvent<>(this));
+        } finally {
             try {
-                RoboGuice.destroyInjector( this );
-            }
-            finally {
+                RoboGuice.destroyInjector(this);
+            } finally {
                 super.onDestroy();
             }
         }
     }
 
     @Override
-    public void onConfigurationChanged( Configuration newConfig ) {
+    public void onConfigurationChanged(Configuration newConfig) {
 
         final Configuration currentConfig = getResources().getConfiguration();
-        super.onConfigurationChanged( newConfig );
-        eventManager.fire( new OnConfigurationChangedEvent( currentConfig, newConfig ) );
+        super.onConfigurationChanged(newConfig);
+        eventManager.fire(new OnConfigurationChangedEvent<>(this, currentConfig, newConfig));
     }
 
     @Override
     public void onSupportContentChanged() {
 
         super.onSupportContentChanged();
-        RoboGuice.getInjector( this ).injectViewMembers( this );
-        eventManager.fire( new OnContentChangedEvent() );
+        RoboGuice.getInjector(this).injectViewMembers(this);
+        eventManager.fire(new OnContentChangedEvent(this));
     }
 
     @Override
-    protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        super.onActivityResult( requestCode, resultCode, data );
-        eventManager.fire( new OnActivityResultEvent( requestCode, resultCode, data ) );
+        super.onActivityResult(requestCode, resultCode, data);
+        eventManager.fire(new OnActivityResultEvent(this, requestCode, resultCode, data));
     }
 
     @Override
